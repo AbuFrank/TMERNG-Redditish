cfea3e4b00a2ceff13d38de27870b73e4cbf8f91
abufrank <riley.thornton1@gmail.com>
2020-12-02 21:48:47 -0500

    added graphcache and fragment regular user

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,77 @@
 import { ChakraProvider, Flex } from "@chakra-ui/react";
 import theme from "@chakra-ui/theme";
-import { cacheExchange } from "@urql/exchange-graphcache";
+import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
 import Layout from "./components/Layout";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "./generated/graphql";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
-  exchanges: [dedupExchange, cacheExchange({}), fetchExchange],
+  // update meQuery cache when login or register queries are run
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 export const App = () => (
